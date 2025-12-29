55a5daa68fc6deea1757566729585400ce12927b
partach <partach@gmail.com>
2025-12-29 22:06:15 +0100

    Update const.py

--- custom_components/ha_modbus_wizard/const.py
+++ custom_components/ha_modbus_wizard/const.py
@@ -19,6 +19,7 @@ CONF_PARITY = "parity"
 CONF_STOPBITS = "stopbits"
 CONF_BYTESIZE = "bytesize"
 CONF_UPDATE_INTERVAL = "update_interval"
+CONF_REGISTERS = "registers"
 # TCP settings
 CONF_HOST = "host"
 CONF_PORT = "port"
