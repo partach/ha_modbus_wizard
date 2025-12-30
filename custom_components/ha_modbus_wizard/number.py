1faabb40d062123141f746df6a56bea0e954abf5
partach <partach@gmail.com>
2025-12-30 17:43:20 +0100

    Update number.py

--- custom_components/ha_modbus_wizard/number.py
+++ custom_components/ha_modbus_wizard/number.py
@@ -2,6 +2,7 @@
 from homeassistant.components.number import NumberEntity
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from .const import DOMAIN
+from homeassistant.helpers.entity import DeviceInfo
 
 async def async_setup_entry(hass, entry, async_add_entities):
     coordinator = hass.data[DOMAIN]["coordinators"][entry.entry_id]
@@ -12,7 +13,18 @@ async def async_setup_entry(hass, entry, async_add_entities):
         if reg.get("rw") != "read" and reg.get("data_type") in ("uint16", "int16", "uint32", "int32", "float32")
     ]
     
-    async_add_entities(entities, True)
+    device_info = DeviceInfo(
+        identifiers={(DOMAIN, entry.entry_id)},
+        name=entry.title or "Modbus Wizard",
+        manufacturer="Partach",
+        model="Wizard",
+        configuration_url=f"homeassistant://config/integrations/integration/{entry.entry_id}",
+    )
+
+    if entities:
+        for entity in entities:
+            entity._attr_device_info = device_info
+        async_add_entities(entities)
 
 class ModbusWizardNumber(CoordinatorEntity, NumberEntity):
     def __init__(self, coordinator, entry, key, info):
