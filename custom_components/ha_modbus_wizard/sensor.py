5b7766f1b356b5eea450cdda7460721f999f5e39
partach <partach@gmail.com>
2025-12-30 12:45:06 +0100

    Update sensor.py

--- custom_components/ha_modbus_wizard/sensor.py
+++ custom_components/ha_modbus_wizard/sensor.py
@@ -3,11 +3,12 @@ from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.components.sensor import SensorEntity
+from .const import DOMAIN,CONF_NAME
 import logging
 _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
-    coordinator = hass.data["modbus_wizard"][entry.entry_id]
+    coordinator = hass.data[DOMAIN]["coordinators"][entry.entry_id]
     
     entities = []
     for reg in entry.options.get("registers", []):
@@ -27,7 +28,12 @@ class ModbusWizardSensor(CoordinatorEntity, SensorEntity):
         self._attr_name = info["name"]
         self._attr_device_class = info.get("device_class")
         self._attr_native_unit_of_measurement = info.get("unit")
-
+        self._attr_device_info = {
+            "identifiers": {(DOMAIN, entry.entry_id)},
+            "name": entry.data.get(CONF_NAME, "Modbus Device"),
+            "manufacturer": "Modbus",
+            "model": "Wizard",
+        }
     @property
     def native_value(self):
         return self.coordinator.data.get(self._key)
